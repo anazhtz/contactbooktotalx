e8105bb3672a3a1520ed86a1b085051b77ab0bb5
Muhammed Anas AK <anazbinashraf2@gmail.com>
2024-08-14 12:30:28 +0530

    main added

---
+++ lib/view/mobile_auth.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MobileAuth extends StatefulWidget {
+  const MobileAuth({super.key});
+
+  @override
+  State<MobileAuth> createState() => _MobileAuthState();
+}
+
+class _MobileAuthState extends State<MobileAuth> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
